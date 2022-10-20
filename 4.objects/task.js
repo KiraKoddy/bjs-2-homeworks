166617cb700ca1b6f6b314581517793f97bee6cb
KiraKoddy <flatcher2007@narod.ru>
2022-10-20 13:28:42 +0300

    initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let kira = new Student("Кира", "женский", 18);
+let sasha = new Student("Саша", "мужской", 22);
+let vanya = new Student("Ваня", "мужской", 19);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = marks;
+  } else {
+    this.marks = this.marks.concat(...marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  if (this.marks === undefined) {
+    return "Нет оценок"
+  } else {
+    for (i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+    }
+    this.average = sum / this.marks.length;
+    return this.average;
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
